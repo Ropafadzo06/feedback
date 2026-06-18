c9dc0e1b84f97529053c74b4d2e9073346b1ebd5
Ropafadzo06 <h230724q@hit.ac.zw>
2026-06-18 09:54:10 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI)
